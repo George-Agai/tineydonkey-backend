4bff5152e50b26cdc6ae41cee293d4ff05244ee4
George Agai <georgeagai2000@gmail.com>
2024-06-13 18:35:42 +0300

    Access middleware revert

--- blockUrlMiddleware.js
+++ blockUrlMiddleware.js
@@ -2,14 +2,19 @@ const blockUrlMiddleware = (req, res, next) => {
   const url = process.env.URL
   const testUrl = process.env.TEST_URL
 
+  const blockedUrls = [
+    'https://sqlmap.org'
+  ];
   const authorizedUrls = []
   authorizedUrls.push(url, testUrl)
   
   const requestedUrl = req.headers.referer || req.headers.origin;
   console.log(requestedUrl)
-  if (!authorizedUrls.includes(requestedUrl)) {
+  if (blockedUrls.includes(requestedUrl)) {
     return res.status(403).json({ error: 'Access forbidden' });
   }
+
+  // Continue with the next middleware
   next();
 };
 
