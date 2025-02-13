7f904d501c31679f228dec43597539a89d6671f5
George Agai <georgeagai2000@gmail.com>
2025-02-13 14:28:47 +0300

    Modify product to generate unique slug and support when fetching

--- TineyDonkeyModels/Product.js
+++ TineyDonkeyModels/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const Product = new mongoose.Schema({
     image: [String],
@@ -13,7 +14,29 @@ const Product = new mongoose.Schema({
     status: {
         type: Boolean,
         default: true
-    }
+    },
+    slug: {
+        type: String,
+        unique: true
+    },
 })
+
+Product.pre("save", async function (next) {
+    let baseSlug = slugify(this.productName, { lower: true, strict: true });
+    let uniqueSlug = baseSlug;
+    let counter = 1;
+
+    // Reference the model dynamically
+    const Model = this.constructor;
+
+    // Check if the slug already exists in the database
+    while (await Model.findOne({ slug: uniqueSlug })) {
+        uniqueSlug = `${baseSlug}-${counter}`;
+        counter++;
+    }
+
+    this.slug = uniqueSlug;
+    next();
+});
 
 module.exports = mongoose.model("Product", Product);
